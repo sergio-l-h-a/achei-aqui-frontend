a85dcd0dcb1d2e3f8c936d30e0d294d8e1fac4e2
Francisco-Sergio <lanhousearte@outlook.com>
2026-07-10 20:15:51 -0300

    fix: Atualizando API

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://achei-aqui-backend.onrender.com",
+  withCredentials: true   // ← obrigatório quando credentials: true no backend
 });
 
 export default api;
